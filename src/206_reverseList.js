0fba512fe74cb7280cb291bae15c5b56906b6c2e
wanzaiwanger <zhmh1025@hotmail.com>
2017-12-07 01:23:41 -0800

    1206-1

---
+++ src/206_reverseList.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * reverse of null: null
+ * reverse of one element: the element
+ * reverse of an n element list: reverse of the second element followed by the first element
+ */
+var reverseListInteration = function (head) {
+    var pre = null;
+    var cur = head;
+    var nxt;
+    while (cur) {
+        nxt = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = nxt;
+    }
+    return pre;
+};
+
+var reverseListRecurrsive = function (head) {
+    var first = head;
+    if (first === null || first.next === null) {
+        return first;
+    }
+    var second = first.next;
+    var reversedSecond = reverseListRecurrsive(second);
+    first.next = null;
+    second.next = first;
+    return reversedSecond;
+};
