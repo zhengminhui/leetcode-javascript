40b0e8a6944a99345dffb517a9df8efc2c54ae2b
wanzaiwanger <zhmh1025@hotmail.com>
2017-12-08 01:11:41 -0800

    1207-1

--- src/1_twoSum.js
+++ src/1_twoSum.js
@@ -3,6 +3,22 @@
  * @param {number} target
  * @return {number[]}
  */
+// O(n)
+var twoSum = function (nums, target) {
+  var map = new Map();
+  var res;
+  nums.forEach((num, i) => {
+    var diff = target - num;
+    if (map.has(diff)) {
+      var index = map.get(diff);
+      res = [index, i];
+    }
+    map.set(num, i);
+  });
+  return res;
+};
+
+// O(n*2)
 var twoSum = function (nums, target) {
   var res = [];
   for (var i = 0; i < nums.length - 1; i++) {
