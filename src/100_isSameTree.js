c8d0f109e7ad6033c298e148b2c3ccb655f05c85
wanzaiwanger <zhmh1025@hotmail.com>
2017-12-17 14:51:28 -0800

    1217_3

--- src/100_isSameTree.js
+++ src/100_isSameTree.js
@@ -11,13 +11,11 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    if (p===null || q===null){
-        return p===q;
-    }
-    else if (p.val != q.val){
-        return false;
-    }
-    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-    
-    
+  if (p === null || q === null) {
+    return p === q;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
