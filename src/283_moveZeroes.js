54804428aa0532bc14d020232e3f163c5b7dfa54
wanzaiwanger <zhmh1025@hotmail.com>
2017-12-23 11:41:24 -0800

    1222_1

---
+++ src/283_moveZeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ * Time Complexity O(n)
+ */
+var moveZeroes = function(nums) {
+  var head = 0;
+  var tail = nums.length - 1;
+  while (head < tail) {
+    if (nums[head] === 0) {
+      // no need to save temp variable, just push 0 to the end
+      nums.splice(head, 1);
+      nums.push(0);
+      tail--;
+      head--;
+    }
+    head++;
+  }
+};
