c3df02d4c1a25be54204cbae87d14f9085aa7137
zhengminhui <zhmh1025@hotmail.com>
2019-01-09 23:57:31 +0800

    add 771

---
+++ src/S0771_Jewels_And_Stones/numJewelsInStones.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+  // let res = 0;
+  // const jewels = new Set(J.split(''));
+  // for (let i=0; i< S.length;  i++) {
+  //   if(jewels.has(S[i])) {
+  //     res += 1;
+  //   }    
+  // }
+  // return res;
+  
+  return S.split('').filter((char) => {
+    return J.includes(char);
+  }).length;
+};
