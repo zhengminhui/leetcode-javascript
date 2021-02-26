37eb72b9bf0cb84293a47d048bebb8922df5df44
zhengminhui <zhmh1025@hotmail.com>
2021-02-26 22:35:20 +0800

    add 3

---
+++ src/leetcode/S0054_spiral-matrix/spiralOrder.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {};
---
+++ src/offer/61.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isStraight = function (nums) {
+    
+};
+
+// const nums = [1, 2, 3, 4, 6];
+// const nums = [2, 3, 4, 5, 6];
+const nums = [0, 0, 1, 2, 5];
+console.log(isStraight(nums));
